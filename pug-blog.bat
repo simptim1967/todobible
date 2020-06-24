e5599a719c9936d483a3b634cbca91217e63ceb9
Tim Simpson <simptim1967@outlook.com>
2020-06-24 23:08:13 +0100

    All Changes blog article

--- pug-blog.bat
+++ pug-blog.bat
@@ -3,6 +3,7 @@ rem Use node.js to generate blog pages
 
 echo Rendering pug blog pages via node.js ...
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\blog-index.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog 
+call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\03-all-change.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\02-signs-of-the-times.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\01-walking-and-talking-with-god.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 
