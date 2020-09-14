df463e8c16a7a9f5b7e9d431f58323e917db2e52
Tim Simpson <simptim1967@outlook.com>
2020-09-14 23:17:01 +0100

    Latest blog article and updates to Ephesians

--- pug-blog.bat
+++ pug-blog.bat
@@ -3,6 +3,7 @@ rem Use node.js to generate blog pages
 
 echo Rendering pug blog pages via node.js ...
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\blog-index.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog 
+call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\11-the-sun-shall-be-turned-into-darkness.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\10-the-love-of-god.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\09-the-heavens-declare-him.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\08-grounded.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
