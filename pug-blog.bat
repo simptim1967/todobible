7d314a78e17066c8170af7e5ed499bbdb8158ead
Tim Simpson <simptim1967@outlook.com>
2021-01-02 00:03:06 +0000

    New blog article and 2021 update

--- pug-blog.bat
+++ pug-blog.bat
@@ -3,6 +3,7 @@ rem Use node.js to generate blog pages
 
 echo Rendering pug blog pages via node.js ...
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\blog-index.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog 
+call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\14-the-greatest-gift.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\13-jesus-at-the-centre.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\12-life-group-challenge.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\11-the-sun-shall-be-turned-into-darkness.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
