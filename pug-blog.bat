049258c6f216238d3abefcd6306bf5ed52fcee70
Tim Simpson <simptim1967@outlook.com>
2021-02-18 23:23:10 +0000

    New blog post learning to walk

--- pug-blog.bat
+++ pug-blog.bat
@@ -2,9 +2,15 @@
 rem Use node.js to generate blog pages
 
 echo Rendering pug blog pages via node.js ...
+rem Blog index
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\blog-index.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog 
+
+rem 2021
+call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2021\17-learning-to-walk.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2021
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2021\16-listen-with-your-eyes.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2021
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2021\15-my-first-vision.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2021
+
+rem 2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\14-the-greatest-gift.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\13-jesus-at-the-centre.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\12-life-group-challenge.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
@@ -20,6 +26,6 @@ call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\03-all-change.pug --pretty 
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\02-signs-of-the-times.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2020\01-walking-and-talking-with-god.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2020
 
-rem Use node.js to generate legacy resource pages
+rem Legacy resource pages
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\resources\the-turner-of-the-pages.pug --pretty --out C:\TIM\Dev\MBP\Source\live\resources 
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\resources\bible-study-ephesians-1.pug --pretty --out C:\TIM\Dev\MBP\Source\live\resources 
