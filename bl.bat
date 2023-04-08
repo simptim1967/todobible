6d03695c2b9e5b11c926b7e41eed76783d27c263
Tim Simpson <simptim1967@outlook.com>
2023-04-08 21:49:46 +0100

    Updated for new article 19

--- bl.bat
+++ bl.bat
@@ -5,6 +5,8 @@ echo Rendering pug blog pages via node.js ...
 rem Blog index
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\blog-index.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog 
 
+rem 2023
+call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2023\19-jesus-on-the-battlefield.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2023
+
 rem 2021
 call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2021\18-always-good-news.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2021
-call Pug C:\TIM\Dev\MBP\Source\WebSite\pug\blog\2021\17-learning-to-walk.pug --pretty --out C:\TIM\Dev\MBP\Source\live\blog\2021
